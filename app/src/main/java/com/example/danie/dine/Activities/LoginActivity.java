56a8234821aa0777653ae65e36e831b60f8bc0fe
Daniel <daniel.czajka@gmail.com>
2018-12-12 18:44:41 +0000

    final project ver 2.0

--- app/src/main/java/com/example/danie/dine/Activities/LoginActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/LoginActivity.java
@@ -1,15 +1,127 @@
 package com.example.danie.dine.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.danie.dine.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText loginEmail;
+    private EditText loginPassword;
+    private Button btnLogin;
+    private ProgressBar progressBarLogin;
+    private Button getBtnLoginExit;
+
+    private FirebaseAuth userAuth;
+
+    private Intent HomeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        getBtnLoginExit = findViewById(R.id.btnLoginExit);
+        progressBarLogin = findViewById(R.id.progressBarLogin);
+        userAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this,com.example.danie.dine.Activities.HomeActivity.class);
+
+        getBtnLoginExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBarLogin.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String loginemail = loginEmail.getText().toString();
+                final String loginpassword = loginPassword.getText().toString();
+
+                if (loginemail.isEmpty() || loginpassword.isEmpty()) {
+                    showMessage("cannot login, please check your input!");
+                }
+                else {
+                    signIn(loginemail, loginpassword);
+                }
+            }
+        });
+
+    }
+
+    private void signIn(String loginemail, String loginpassword) {
+
+        userAuth.signInWithEmailAndPassword(loginemail, loginpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    progressBarLogin.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+                else {
+
+                    showMessage(task.getException().getMessage());
+                    progressBarLogin.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+        });
+
+    }
+
+
+    private void updateUI() {
+
+        startActivity(HomeActivity);
+        finish();
+
+    }
+
+    private void showMessage(String s) {
+        Toast.makeText(getApplicationContext(), s,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //check if any user is already logged in
+        FirebaseUser user = userAuth.getCurrentUser();
+
+        if (user != null) {
+            //if user is already logged in, we need to redirect to home page
+            updateUI();
+
+        }
     }
 }
--- app/src/main/java/com/example/danie/dine/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.danie.dine.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,15 +11,25 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.danie.dine.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
-    private EditText userName,userEmail,userPhone,userPassword1,userPassword2;
+    private EditText userName;
+    private EditText userEmail;
+    private EditText userPhone;
+    private EditText userPassword1;
+    private EditText userPassword2;
     private ProgressBar progressBarRegister;
     private Button btnRegConfirm;
 
+    //firebase authentication
     private FirebaseAuth userAuth;
 
     @Override
@@ -30,12 +42,14 @@ public class RegisterActivity extends AppCompatActivity {
         userPhone = findViewById(R.id.regPhone);
         userPassword1 = findViewById(R.id.regPassword1);
         userPassword2 = findViewById(R.id.regPassword2);
+        progressBarRegister = findViewById(R.id.progressBarRegister);
 
         btnRegConfirm = findViewById(R.id.btnRegConfirm);
         progressBarRegister.setVisibility(View.INVISIBLE);
 
         userAuth = FirebaseAuth.getInstance();
 
+        ///*** register button***///
         btnRegConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,6 +63,20 @@ public class RegisterActivity extends AppCompatActivity {
                 final String userphone = userPhone.getText().toString();
 
 
+                if (email.isEmpty() || username.isEmpty() || userphone.isEmpty() || password1.isEmpty() || !password1.equals(password2)) {
+                    //user cannot be registered, something wrong
+                    showMessage("Registration Failed, pelase check your details!");
+                    btnRegConfirm.setVisibility(View.VISIBLE);
+                    progressBarRegister.setVisibility(View.INVISIBLE);
+
+
+                }
+                else {
+                    //registration successful
+                    CreateUserAccount(email, username, password1);
+                }
+
+
             }
         });
 
@@ -56,11 +84,65 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    private void CreateUserAccount(String email, final String username, String password1) {
+
+        //register new user method
+        userAuth.createUserWithEmailAndPassword(email, password1)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            //user account is created successfully
+                            showMessage("Account Created!");
+                            //once user account created succesfully, user details needs to be updated
+                            updateUserInfo(username,userAuth.getCurrentUser());
+
+                        }
+                        else {
+                            //account not created, something wrong
+                            showMessage("cannot create account, check your details" + task.getException().getMessage().toString());
+                            btnRegConfirm.setVisibility(View.VISIBLE);
+                            progressBarRegister.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUserInfo(String username, FirebaseUser currentUser) {
+
+        //update user name method
+        //StorageReference userStorage = FirebaseStorage.getInstance().getReference().child("")
+        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .build();
+        currentUser.updateProfile(profileUpdate)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful()) {
+                            //user info updated successfully
+                            showMessage("Registration Complete!");
+                            updateUI();
+                        }
+
+                    }
+                });
+    }
+
+    private void updateUI() {
+        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeActivity);
+        finish();
+
+    }
 
 
-    //showing toast message
     private void showMessage(String message) {
 
+        //showing toast message method
         Toast.makeText(getApplicationContext(), message,Toast.LENGTH_LONG).show();
 
     }
