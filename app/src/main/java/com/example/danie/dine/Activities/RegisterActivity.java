98819a1e165cb7441a2cf9021a48ca4e9e975036
Daniel <daniel.czajka@gmail.com>
2018-12-12 14:20:41 +0000

    final project

--- app/src/main/java/com/example/danie/dine/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/RegisterActivity.java
@@ -2,16 +2,23 @@ package com.example.danie.dine.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.danie.dine.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
     private EditText userName,userEmail,userPhone,userPassword1,userPassword2;
-    private ProgressBar loadingProgress;
+    private ProgressBar progressBarRegister;
+    private Button btnRegConfirm;
+
+    private FirebaseAuth userAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,38 @@ public class RegisterActivity extends AppCompatActivity {
         userPhone = findViewById(R.id.regPhone);
         userPassword1 = findViewById(R.id.regPassword1);
         userPassword2 = findViewById(R.id.regPassword2);
+
+        btnRegConfirm = findViewById(R.id.btnRegConfirm);
+        progressBarRegister.setVisibility(View.INVISIBLE);
+
+        userAuth = FirebaseAuth.getInstance();
+
+        btnRegConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnRegConfirm.setVisibility(View.INVISIBLE);
+                progressBarRegister.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password1 = userPassword1.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+                final String username = userName.getText().toString();
+                final String userphone = userPhone.getText().toString();
+
+
+            }
+        });
+
+
+
+    }
+
+
+
+    //showing toast message
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(), message,Toast.LENGTH_LONG).show();
 
     }
 }
--- app/src/main/java/com/example/danie/dine/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.danie.dine.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.danie.dine.R;
 
@@ -11,5 +14,34 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnWelcomeLogin = (Button)findViewById(R.id.btnWelcomeLogin);
+        btnWelcomeLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent welcomeLoginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                //
+                startActivity(welcomeLoginIntent);
+            }
+        });
+
+        Button btnWelcomeRegister = (Button)findViewById(R.id.btnWelcomeRegister);
+        btnWelcomeRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent welcomeRegisterIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                //
+                startActivity(welcomeRegisterIntent);
+            }
+        });
+
+        Button btnWelcomeExit = (Button) findViewById(R.id.btnWelcomeExit);
+        btnWelcomeExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
