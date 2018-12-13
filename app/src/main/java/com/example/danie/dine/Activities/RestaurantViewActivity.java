299e7f58b6a6cd0f54c27235c9704077c794b201
Daniel <daniel.czajka@gmail.com>
2018-12-13 21:05:57 +0000

    adding firebase realtime database

---
+++ app/src/main/java/com/example/danie/dine/Activities/RestaurantViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.danie.dine.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.danie.dine.R;
+
+public class RestaurantViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_view);
+    }
+}
