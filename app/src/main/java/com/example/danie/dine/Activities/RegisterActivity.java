a7687d90c401ebea407a9789378f9d0d189f8053
Daniel <daniel.czajka@gmail.com>
2019-02-24 22:18:59 +0000

    trying to add authentication to app

--- app/src/main/java/com/example/danie/dine/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.danie.dine.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,10 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.danie.dine.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,11 +29,18 @@ public class RegisterActivity extends AppCompatActivity {
     private Button btnRegConfirm;
 
 
+    //firebase
+    private FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         regFirstName = findViewById(R.id.regFirstName);
         regLogin = findViewById(R.id.regLogin);
@@ -54,36 +66,7 @@ public class RegisterActivity extends AppCompatActivity {
 
 
 
-                        //check if user name is empty
-                        if (regLogin.getText().toString().isEmpty()) {
-                            showMessage("please fill in username!");
-                            btnRegConfirm.setVisibility(View.VISIBLE);
-                            progressBarRegister.setVisibility(View.INVISIBLE);
-                        }
-                        //checking if email.is empty
-                        else if (regEmail.getText().toString().isEmpty()) {
-                            showMessage("please fill in email address!");
-                            btnRegConfirm.setVisibility(View.VISIBLE);
-                            progressBarRegister.setVisibility(View.INVISIBLE);
-                        }
-                        //checking if phone is empty
-                        else if (regPhone.getText().toString().isEmpty()) {
-                            showMessage("please fill in phone number!");
-                            btnRegConfirm.setVisibility(View.VISIBLE);
-                            progressBarRegister.setVisibility(View.INVISIBLE);
-                        }
-                        //checking if password is empty
-                        else if (regPassword1.getText().toString().isEmpty()) {
-                            showMessage("please fill in password!");
-                            btnRegConfirm.setVisibility(View.VISIBLE);
-                            progressBarRegister.setVisibility(View.INVISIBLE);
-                        }
-                        //check if password1 match password2
-                        else if (!regPassword1.getText().toString().equals(regPassword2.getText().toString())) {
-                            showMessage("password does not match!");
-                            btnRegConfirm.setVisibility(View.VISIBLE);
-                            progressBarRegister.setVisibility(View.INVISIBLE);
-                        }
+
 
                         }
 
@@ -94,6 +77,58 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         });
+
+    }
+
+    private void registerNewUser(){
+
+
+        //get input
+        regLogin.getText().toString().trim();
+        regEmail.getText().toString().trim();
+        regPhone.getText().toString().trim();
+        regPassword1.getText().toString().trim();
+        regPassword2.getText().toString().trim();
+
+        //check if user name is empty
+        if (regLogin.getText().toString().isEmpty()) {
+            showMessage("please fill in username!");
+            btnRegConfirm.setVisibility(View.VISIBLE);
+            progressBarRegister.setVisibility(View.INVISIBLE);
+        }
+        //checking if email.is empty
+        else if (regEmail.getText().toString().isEmpty()) {
+            showMessage("please fill in email address!");
+            btnRegConfirm.setVisibility(View.VISIBLE);
+            progressBarRegister.setVisibility(View.INVISIBLE);
+        }
+        //checking if phone is empty
+        else if (regPhone.getText().toString().isEmpty()) {
+            showMessage("please fill in phone number!");
+            btnRegConfirm.setVisibility(View.VISIBLE);
+            progressBarRegister.setVisibility(View.INVISIBLE);
+        }
+        //checking if password is empty
+        else if (regPassword1.getText().toString().isEmpty()) {
+            showMessage("please fill in password!");
+            btnRegConfirm.setVisibility(View.VISIBLE);
+            progressBarRegister.setVisibility(View.INVISIBLE);
+        }
+        //check if password1 match password2
+        else if (!regPassword1.getText().toString().equals(regPassword2.getText().toString())) {
+            showMessage("password does not match!");
+            btnRegConfirm.setVisibility(View.VISIBLE);
+            progressBarRegister.setVisibility(View.INVISIBLE);
+        }
+
+
+        firebaseAuth.createUserWithEmailAndPassword(regEmail, regPassword1)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    }
+                });
 
     }
 
