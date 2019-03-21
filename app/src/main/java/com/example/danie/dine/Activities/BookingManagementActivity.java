f0e8231cdea726b310e8dfd163aa508084120391
Daniel <daniel.czajka@gmail.com>
2019-03-21 22:46:37 +0000

    adding time picker

--- app/src/main/java/com/example/danie/dine/Activities/BookingManagementActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/BookingManagementActivity.java
@@ -1,6 +1,7 @@
 package com.example.danie.dine.Activities;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,14 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.danie.dine.Model.DatePickerFragment;
+import com.example.danie.dine.Model.TimePickerFragment;
 import com.example.danie.dine.R;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class BookingManagementActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class BookingManagementActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
 
 
@@ -27,13 +30,21 @@ public class BookingManagementActivity extends AppCompatActivity implements Date
 
         Button btnDate = (Button) findViewById(R.id.btnDate);
         Button btnTime = (Button) findViewById(R.id.btnTime);
-        TextView lblTime = (TextView) findViewById(R.id.lblTime);
+
 
         btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DialogFragment datePicker = new DatePickerFragment();
                 datePicker.show(getSupportFragmentManager(), "Date");
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "Time");
             }
         });
     }
@@ -49,6 +60,18 @@ public class BookingManagementActivity extends AppCompatActivity implements Date
         //lblDate.setVisibility(View.VISIBLE);
         lblDate.setText(currentDate);
 
+
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        String currentTime = DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime());
+        TextView lblTime = (TextView) findViewById(R.id.lblTime);
+        lblTime.setText(currentTime);
 
 
     }
