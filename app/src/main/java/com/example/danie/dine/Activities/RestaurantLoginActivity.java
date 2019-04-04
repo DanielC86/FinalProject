f4c78cafa00e12145040339ca5ede84b04e9ff65
Daniel <daniel.czajka@gmail.com>
2019-04-04 22:29:56 +0100

    add restaurant login functionality plus basic error trapping for it

--- app/src/main/java/com/example/danie/dine/Activities/RestaurantLoginActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/RestaurantLoginActivity.java
@@ -1,25 +1,42 @@
 package com.example.danie.dine.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.danie.dine.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RestaurantLoginActivity extends AppCompatActivity {
 
 
-    private TextView rEmail;
-    private TextView rPassword;
+    private EditText rEmail;
+    private EditText rPassword;
     private Button btnRLogin;
     private Button btnRExit;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //check if user already logged in
+        if (mAuth.getCurrentUser() != null){
+            //start home activity
+            RestaurantLogin();
+        }
 
         rEmail = findViewById(R.id.rEmail);
         rPassword = findViewById(R.id.rPassword);
@@ -49,5 +66,43 @@ public class RestaurantLoginActivity extends AppCompatActivity {
 
     private void RestaurantLogin(){
         //method for restaurant login
+        //get user input
+        String restaurantEmail = rEmail.getText().toString().trim();
+        String restaurantPassword = rPassword.getText().toString().trim();
+
+        //check if the fields are empty
+        if (restaurantEmail.isEmpty()) {
+            showMessage("Please fill in email address!");
+            btnRLogin.setVisibility(View.VISIBLE);
+            //progressBarLogin.setVisibility(View.INVISIBLE);
+        }
+        else if (restaurantPassword.isEmpty()) {
+            showMessage("Please fill in password!");
+            btnRLogin.setVisibility(View.VISIBLE);
+            //progressBarLogin.setVisibility(View.INVISIBLE);
+        }
+        else {
+            //user login
+            mAuth.signInWithEmailAndPassword(restaurantEmail, restaurantPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                updateUI();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    //messaging method
+    private void showMessage(String s) {
+        Toast.makeText(getApplicationContext(), s,Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateUI(){
+        Intent RestaurantViewActivity = new Intent (getApplicationContext(), RestaurantViewActivity.class);
+        startActivity(RestaurantViewActivity);
     }
 }
