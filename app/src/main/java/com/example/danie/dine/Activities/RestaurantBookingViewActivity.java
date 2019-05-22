5ea98efe50faa532aca5dbff139812bc95993516
Daniel <daniel.czajka@gmail.com>
2019-05-22 23:25:08 +0100

    restaurant small menu drawer added, restaurant logoff option added (right on time!)

--- app/src/main/java/com/example/danie/dine/Activities/RestaurantBookingViewActivity.java
+++ app/src/main/java/com/example/danie/dine/Activities/RestaurantBookingViewActivity.java
@@ -1,9 +1,13 @@
 package com.example.danie.dine.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -204,5 +208,33 @@ public class RestaurantBookingViewActivity extends AppCompatActivity {
 
     }
 
+    private void logoff() {
+        tableAuth.signOut();
+        Intent restaurantLoginActivity = new Intent(getApplicationContext(), RestaurantLoginActivity.class);
+        startActivity(restaurantLoginActivity);
+        finish();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.restaurant_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.item1:
+                return true;
+            case R.id.item2:
+                return true;
+            case R.id.item3:
+                logoff();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
+
+    }
 }
